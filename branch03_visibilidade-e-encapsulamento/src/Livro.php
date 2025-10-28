d9c99eea8f140ea40d6eff83eab5f04ecf12e58f
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-10-28 16:03:31 -0300

    Update

--- branch03_visibilidade-e-encapsulamento/src/Livro.php
+++ branch03_visibilidade-e-encapsulamento/src/Livro.php
@@ -1,26 +1,53 @@
 <?php
 class Livro {
-    public string $titulo;
-    public string $autor;
-    public ?int $paginas;
+    private string $titulo;
+    private string $autor;
+    private ?int $paginas;
 
     public function __construct(
         string $valorTitulo, 
         string $valorAutor,
         ?int $valorPaginas = null
     ){
-        $this->titulo = $valorTitulo;
-        $this->autor = $valorAutor;
-        $this->paginas = $valorPaginas;
+        $this->setValorTitulo($valorTitulo);
+        $this->setAutor($valorAutor);
+        $this->setTotalPaginas($valorPaginas);
     }
 
     /* programe também (dentro da classe Livro) um método chamado verificarTitulo que, ao ser chamado, ele verifica se a quantidade de caracteres do atributo "titulo" é menor que 3. E se for, o método deve mostrar uma mensagem vermelha dizendo "Título não pode ter menos do que 3 letras */
-    public function verificarTitulo():void {
+    public function verificarTitulo(string $b):string {
 
-        if( mb_strlen($this->titulo) < 3 ){
+        if( mb_strlen($b) < 3 ){
             echo "<p style='color:red'>
             Título não pode ter menos do que 3 letras</p>";
         }
 
+        return $b
     }
+
+    public function getTitulo():string{
+        return $this->titulo;
+    }
+
+    public function getAutor():string{
+        return $this->autor;
+    }
+
+    public function getPaginas():int{
+        return $this->paginas;
+    }
+
+    private function setValorTitulo(float $valorTitulo):void{
+        $a = $this->verificarTitulo($valorTitulo);
+        $this->valorTitulo = $valorTitulo;
+    }
+
+    private function setAutor(string $autor):void {
+        $this->autor = $autor;
+    }
+
+    private function setTotalPaginas(int $valorPaginas):void {
+        $this->totalPaginas = $valorPaginas;
+    }
+
 }
